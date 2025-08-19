8aa4cc6fa7fa967c7360659e52b976acfca9892d
DasankaSliit <it24103499@my.sliit.lk>
2025-08-19 16:50:43 +0530

    Add files via upload
    Files for the backend

---
+++ y2s1se2012project/src/main/java/com/ooadProjectse2012/y2s1se2012project/Y2s1se2012projectApplication.java
@@ -0,0 +1,13 @@
+package com.ooadProjectse2012.y2s1se2012project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2s1se2012projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y2s1se2012projectApplication.class, args);
+	}
+
+}
---
+++ y2s1se2012project/src/test/java/com/ooadProjectse2012/y2s1se2012project/Y2s1se2012projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooadProjectse2012.y2s1se2012project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y2s1se2012projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
